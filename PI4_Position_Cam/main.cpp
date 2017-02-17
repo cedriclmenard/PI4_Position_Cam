c0a23675ef5d96d8cca13b379da6911fdc288c8a
Cedric Leblond Menard <cedlmen@hotmail.com>
2017-02-17 10:51:06 -0500

    Initial Commit

---
+++ PI4_Position_Cam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PI4_Position_Cam
+//
+//  Created by Cedric Leblond Menard on 17-02-17.
+//  Copyright © 2017 AER4875. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
